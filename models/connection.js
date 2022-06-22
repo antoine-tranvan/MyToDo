cb30f2919b4055864227a3da1541c1930e0d7f37
antoine-tranvan <antoine.tranvan@gmail.com>
2022-06-22 15:09:22 +0200

    dot env mongo

--- models/connection.js
+++ models/connection.js
@@ -1,4 +1,6 @@
 var mongoose = require("mongoose");
+var dotenv = require("dotenv");
+dotenv.config();
 
 var options = {
   connectTimeoutMS: 5000,
@@ -7,7 +9,7 @@ var options = {
 };
 
 mongoose.connect(
-  "mongodb+srv://antoinetranvan:Peterpan10!@cluster0.d792e.mongodb.net/ToDoList?retryWrites=true&w=majority",
+  `mongodb+srv://antoinetranvan:${process.env.ATLAS_PWD}@cluster0.d792e.mongodb.net/ToDoList?retryWrites=true&w=majority`,
   options,
   function (error) {
     if (error == null) {
