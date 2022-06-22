46eb3692cdf1b9b00b1247935dd78268a49bb047
antoine-tranvan <antoine.tranvan@gmail.com>
2022-06-22 17:48:36 +0200

    change user

--- models/connection.js
+++ models/connection.js
@@ -9,7 +9,7 @@ var options = {
 };
 
 mongoose.connect(
-  `mongodb://antoinetranvan:${process.env.ATLAS_PWD}@cluster0-shard-00-00.d792e.mongodb.net:27017,cluster0-shard-00-01.d792e.mongodb.net:27017,cluster0-shard-00-02.d792e.mongodb.net:27017/ToDoList?ssl=true&replicaSet=atlas-1047k8-shard-0&authSource=admin&retryWrites=true&w=majority`,
+  `mongodb://dbUser:${process.env.ATLAS_PWD}@cluster0-shard-00-00.d792e.mongodb.net:27017,cluster0-shard-00-01.d792e.mongodb.net:27017,cluster0-shard-00-02.d792e.mongodb.net:27017/ToDoList?ssl=true&replicaSet=atlas-1047k8-shard-0&authSource=admin&retryWrites=true&w=majority`,
   options,
   function (error) {
     if (error == null) {
